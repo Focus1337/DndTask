f458384aa2b6afe57d20a89994483577b20b601e
Focus <maniaclord@bk.ru>
2022-01-08 00:54:27 +0300

    Переделал некоторые моменты. Нужно переделать: CharacterModel и CalculatedCharacterModel

--- DndTaskBusiness/Services/Characters.cs
+++ DndTaskBusiness/Services/Characters.cs
@@ -1,22 +1,46 @@
 namespace DndTaskBusiness.Services;
 
-public record CharacterModel(
-    string Name,
-    int AttackModifier,
-    int AttackPerRound,
-    int DamageDicesCount,
-    int DamageDiceType,
-    int WeaponModifier,
-    int AttackModifierAddition,
-    int DamageModifierAddition);
+// public record CharacterModel(
+//     string Name,
+//     int AttackModifier,
+//     int AttackPerRound,
+//     int DamageDicesCount,
+//     int DamageDiceType,
+//     int WeaponModifier,
+//     int AttackModifierAddition,
+//     int DamageModifierAddition);
+//
+// public record CalculatedCharacterModel(
+//     string Name,
+//     int AttackModifier,
+//     int AttackPerRound,
+//     int DamageDicesCount,
+//     int DamageDiceType,
+//     int WeaponModifier,
+//     int MinAcToAlwaysHit,
+//     int DamagePerRoundLeft,
+//     int DamagePerRoundRight);
 
-public record CalculatedCharacterModel(
-    string Name,
-    int AttackModifier,
-    int AttackPerRound,
-    int DamageDicesCount,
-    int DamageDiceType,
-    int WeaponModifier,
-    int MinAcToAlwaysHit,
-    int DamagePerRoundLeft,
-    int DamagePerRoundRight);
+public class CharacterBase
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public int AttackModifier { get; set; }
+    public int AttackPerRound { get; set; }
+    public int DamageDicesCount { get; set; }
+    public int DamageDiceType { get; set; }
+    public int WeaponModifier { get; set; }   
+}
+
+public class CharacterModel : CharacterBase
+{
+    public int AttackModifierAddition { get; set; }
+    public int DamageModifierAddition { get; set; }
+}
+
+public class CalculatedCharacterModel : CharacterBase
+{
+    public int MinAcToAlwaysHit { get; set; }
+    public int DamagePerRoundLeft { get; set; }
+    public int DamagePerRoundRight { get; set; }
+}
--- DndTaskUi/Pages/Result.cshtml.cs
+++ DndTaskUi/Pages/Result.cshtml.cs
@@ -5,31 +5,9 @@ namespace DndTaskUi.Pages;
 
 public class Result : PageModel
 {
-    public CalculatedCharacterModel Character { get; set; }
+    public CalculatedCharacterModel Character { get; set; } = null!;
 
     private static readonly HttpClient client = new();
-
-    private record CharacterModel(
-        string Name,
-        int AttackModifier,
-        int AttackPerRound,
-        int DamageDicesCount,
-        int DamageDiceType,
-        int WeaponModifier,
-        int AttackModifierAddition,
-        int DamageModifierAddition);
-
-
-    public record CalculatedCharacterModel(
-        string Name,
-        int AttackModifier,
-        int AttackPerRound,
-        int DamageDicesCount,
-        int DamageDiceType,
-        int WeaponModifier,
-        int MinAcToAlwaysHit,
-        int DamagePerRoundLeft,
-        int DamagePerRoundRight);
 
     public void OnGet()
     {
@@ -37,56 +15,44 @@ public class Result : PageModel
         var attack = int.Parse(Request.Query["attack"]);
         var weapon = int.Parse(Request.Query["weapon"]);
         var content = client.GetAsync($"https://localhost:7049/GetCharacterById?id={id}").Result.Content;
+
         Console.WriteLine(content.ReadAsStringAsync().Result);
+
         var character = content.ReadFromJsonAsync<Character>().Result!;
-        var characterModel = new CharacterModel(
-            Name: character.Name,
-            AttackModifier: character.AttackModifier,
-            AttackPerRound: character.AttackPerRound,
-            DamageDicesCount: character.DamageDicesCount,
-            DamageDiceType: character.DamageDiceType,
-            WeaponModifier: character.WeaponModifier,
-            AttackModifierAddition: attack,
-            DamageModifierAddition: weapon);
+
+        var characterModel = new CharacterModel
+        {
+            Name = character.Name,
+            AttackModifier = character.AttackModifier,
+            AttackPerRound = character.AttackPerRound,
+            DamageDicesCount = character.DamageDicesCount,
+            DamageDiceType = character.DamageDiceType,
+            WeaponModifier = character.WeaponModifier,
+            AttackModifierAddition = attack,
+            DamageModifierAddition = weapon
+        };
+
         content = client
             .PostAsync($"https://localhost:7198/CalculateCharacterProperties", JsonContent.Create(characterModel))
             .Result.Content;
+
         Console.WriteLine(content.ReadAsStringAsync().Result);
         Character = content.ReadFromJsonAsync<CalculatedCharacterModel>().Result!;
     }
 }
 
-// public class Result : PageModel
-// {
-//     public Character Character { get; set; } = null!;
-//
-//     private static readonly HttpClient client = new();
-//     
-//     public void OnGet()
-//     {
-//         var id = Request.Query["id"];
-//         var attack = int.Parse(Request.Query["attack"]);
-//         var weapon = int.Parse(Request.Query["weapon"]);
-//         var content = client.GetAsync($"https://localhost:7049/GetCharacterById?id={id}").Result.Content;
-//         
-//         Console.WriteLine(content.ReadAsStringAsync().Result);
-//         
-//         var character = content.ReadFromJsonAsync<Character>().Result!;
-//         var characterModel = new CharacterModel(
-//             Name: character.Name,
-//             AttackModifier: character.AttackModifier,
-//             AttackPerRound: character.AttackPerRound,
-//             DamageDicesCount: character.DamageDicesCount,
-//             DamageDiceType: character.DamageDiceType,
-//             WeaponModifier: character.WeaponModifier,
-//             AttackModifierAddition: attack,
-//             DamageModifierAddition: weapon);
-//        
-//         content = client
-//             .PostAsync($"https://localhost:7198/CalculateCharacterProperties", JsonContent.Create(characterModel))
-//             .Result.Content;
-//         
-//         Console.WriteLine(content.ReadAsStringAsync().Result);
-//         Character = content.ReadFromJsonAsync<CalculatedCharacterModel>().Result!;
-//     }
-// }
+public class CharacterBase : Character
+{ }
+
+internal class CharacterModel : CharacterBase
+{
+    public int AttackModifierAddition { get; set; }
+    public int DamageModifierAddition { get; set; }
+}
+
+public class CalculatedCharacterModel : CharacterBase
+{
+    public int MinAcToAlwaysHit { get; set; }
+    public int DamagePerRoundLeft { get; set; }
+    public int DamagePerRoundRight { get; set; }
+}
